43edd230fc2f2a7ceb74a76fb520c0bba9501cf8
Hyiker Hu <961531608@qq.com>
2021-07-02 23:04:03 +0800

    add regfile

---
+++ src/main/scala/com/github/hectormips/RegFile.scala
@@ -0,0 +1,40 @@
+package com.github.hectormips
+
+import chisel3._
+import chisel3.util._
+import chisel3.stage.{ChiselGeneratorAnnotation, ChiselStage}
+
+// 寄存器堆文件
+class RegFile extends Module {
+  val io: Bundle {
+    val raddr1: UInt
+    val rdata1: UInt
+    val raddr2: UInt
+    val rdata2: UInt
+    val we: Bool
+    val waddr: UInt
+    val wdata: UInt
+  } = IO(new Bundle {
+    val raddr1: UInt = Input(UInt(5.W))
+    val rdata1: UInt = Output(UInt(32.W))
+
+    val raddr2: UInt = Input(UInt(5.W))
+    val rdata2: UInt = Output(UInt(32.W))
+
+    val we: Bool = Input(Bool())
+    val waddr: UInt = Input(UInt(5.W))
+    val wdata: UInt = Input(UInt(32.W))
+  })
+  val regs = Reg(Vec(32, UInt(32.W)))
+  regs(0) := 0.U
+  when(io.we.asBool() && io.waddr > 0.U) {
+    regs(io.waddr) := io.wdata
+  }
+  io.rdata1 := regs(io.raddr1)
+  io.rdata2 := regs(io.raddr2)
+
+}
+
+object RegFile extends App {
+  (new ChiselStage).emitVerilog(new RegFile)
+}
---
+++ src/test/scala/com/github/hectormips/RegFileTest.scala
@@ -0,0 +1,32 @@
+package com.github.hectormips
+
+import chisel3._
+import chiseltest._
+import org.scalatest._
+import chiseltest.experimental.TestOptionBuilder._
+import chiseltest.internal.WriteVcdAnnotation
+
+class RegFileTest extends FlatSpec with ChiselScalatestTester with Matchers {
+  behavior of "RegFile"
+
+  it should "write and read" in {
+    test(new RegFile()).withAnnotations(Seq(WriteVcdAnnotation)) { c =>
+      for (i <- 0 until 31) {
+        c.io.we.poke(true.B)
+        c.io.waddr.poke(i.U)
+        c.io.wdata.poke((i * 13 + 1).U)
+        c.clock.step(1)
+        if (i > 1) {
+          c.io.raddr1.poke((i - 1).U)
+          c.io.rdata1.expect(((i - 1) * 13 + 1).U)
+        }
+      }
+      c.io.we.poke(true.B)
+      c.io.waddr.poke(0.U)
+      c.io.wdata.poke(114.U)
+      c.clock.step(1)
+      c.io.raddr2.poke(0.U)
+      c.io.raddr2.expect(0.U)
+    }
+  }
+}
