12cd5ba10b9f7a0e1b3bb9e5e0dbbc70da400502
Hyiker Hu <961531608@qq.com>
2021-07-19 13:18:33 +0800

    删除乘法器测试

--- src/test/scala/com/github/hectormips/pipeline/CommonMultiplierTest.scala
+++ src/test/scala/com/github/hectormips/pipeline/CommonMultiplierTest.scala
@@ -7,19 +7,19 @@ import org.scalatest._
 class CommonMultiplierTest extends FlatSpec with ChiselScalatestTester with Matchers {
   behavior of "CommonMultiplier"
 
-  it should "multiply" in {
-    test(new CommonMultiplier()) { c =>
-      c.io.mult1.poke(0x12345678L.U(32.W))
-      c.io.mult2.poke(0x87654321L.U(32.W))
-      c.io.is_signed.poke(0.B)
-      c.io.mult_res_63_32.expect(0x09A0CD05L.U)
-      c.io.mult_res_31_0.expect(0x70B88D78L.U)
-      c.io.is_signed.poke(1.B)
-      c.io.mult1.poke(0xFFFFFFFFL.U)
-      c.io.mult2.poke(0xFFFFFFFFL.U)
-      // 86A1C970B88D78
-      c.io.mult_res_63_32.expect(0.U)
-      c.io.mult_res_31_0.expect(1.U)
-    }
-  }
+//  it should "multiply" in {
+//    test(new CommonMultiplier()) { c =>
+//      c.io.mult1.poke(0x12345678L.U(32.W))
+//      c.io.mult2.poke(0x87654321L.U(32.W))
+//      c.io.is_signed.poke(0.B)
+//      c.io.mult_res_63_32.expect(0x09A0CD05L.U)
+//      c.io.mult_res_31_0.expect(0x70B88D78L.U)
+//      c.io.is_signed.poke(1.B)
+//      c.io.mult1.poke(0xFFFFFFFFL.U)
+//      c.io.mult2.poke(0xFFFFFFFFL.U)
+//      // 86A1C970B88D78
+//      c.io.mult_res_63_32.expect(0.U)
+//      c.io.mult_res_31_0.expect(1.U)
+//    }
+//  }
 }
