55ddecb2696ce44f71f7b73e92afe73092ddabf2
ruiqurm <ruiqurm@gmail.com>
2021-08-01 18:49:29 +0800

    icache预取器

--- src/main/scala/com/github/hectormips/cache/icache/ICache.scala
+++ src/main/scala/com/github/hectormips/cache/icache/ICache.scala
@@ -9,28 +9,28 @@ import chisel3.stage.{ChiselGeneratorAnnotation, ChiselStage}
 import chisel3.util.experimental.{forceName}
 
 
-class ICache(val config:CacheConfig)
- extends Module{
-  var io = IO(new Bundle{
-    val valid   = Input(Bool())
-    val addr    = Input(UInt(32.W)) //等到ok以后才能撤去数据
+class ICache(val config: CacheConfig)
+  extends Module {
+  var io = IO(new Bundle {
+    val valid = Input(Bool())
+    val addr = Input(UInt(32.W)) //等到ok以后才能撤去数据
     val addr_ok = Output(Bool()) //等到ok以后才能撤去数据
 
-    val inst   = Output(UInt(64.W))
+    val inst = Output(UInt(64.W))
     val instOK = Output(Bool())
 
-    val instValid =Output(UInt())
+    val instValid = Output(UInt())
 
-    val axi     = new Bundle{
-      val readAddr  =  Decoupled(new AXIAddr(32,4))
-      val readData  = Flipped(Decoupled(new AXIReadData(32,4)))
+    val axi = new Bundle {
+      val readAddr = Decoupled(new AXIAddr(32, 4))
+      val readData = Flipped(Decoupled(new AXIReadData(32, 4)))
     }
 
-    val debug_total_count = Output(UInt(32.W))  // cache总查询次数
-    val debug_hit_count   = Output(UInt(32.W))  // cache命中数
+    val debug_total_count = Output(UInt(32.W)) // cache总查询次数
+    val debug_hit_count = Output(UInt(32.W)) // cache命中数
   })
-  val sIDLE::sLOOKUP::sREPLACE::sREFILL::sQueryPrefetch::sWaitPrefetch::sReqPrefetch::Nil =Enum(7)
-  val state = RegInit(0.U(3.W))// 初始化阶段
+  val sIDLE :: sLOOKUP :: sREPLACE :: sREFILL :: sQueryPrefetch :: sWaitPrefetch  :: Nil = Enum(6)
+  val state = RegInit(0.U(3.W)) // 初始化阶段
   val prefetch = Module(new Prefetch(config))
 
 
@@ -43,12 +43,12 @@ class ICache(val config:CacheConfig)
     }
   }
   val tagvMem = List.fill(config.wayNum) {
-    Module(new icache_tagv(config.tagWidth,config.lineNum))
+    Module(new icache_tagv(config.tagWidth, config.lineNum))
   }
 
-//  val validMem = RegInit(VecInit(Seq.fill(config.wayNum)(VecInit(Seq.fill(config.lineNum)(false.B)))))
+  //  val validMem = RegInit(VecInit(Seq.fill(config.wayNum)(VecInit(Seq.fill(config.lineNum)(false.B)))))
 
-  val lruMem = Module(new LruMem(config.wayNumWidth,config.indexWidth))// lru
+  val lruMem = Module(new LruMem(config.wayNumWidth, config.indexWidth)) // lru
 
   val cache_hit_onehot = Wire(Vec(config.wayNum, Bool())) // 命中的路
   val cache_hit_way = Wire(UInt(config.wayNumWidth.W))
@@ -59,7 +59,7 @@ class ICache(val config:CacheConfig)
 
 
   val dataMemEn = RegInit(false.B)
-  val bDataWtBank = RegInit(0.U((config.offsetWidth-2).W))
+  val bDataWtBank = RegInit(0.U((config.offsetWidth - 2).W))
   val AXI_readyReg = RegInit(false.B)
 
   val is_hitWay = Wire(Bool())
@@ -67,14 +67,14 @@ class ICache(val config:CacheConfig)
 
 
   val addrokReg = RegInit(false.B)
-//  io.addr_ok := state === sIDLE
+  //  io.addr_ok := state === sIDLE
   io.addr_ok := state === sIDLE || (state === sLOOKUP && is_hitWay)
 
   state := sIDLE
 
-  val index  = Wire(UInt(config.indexWidth.W))
-  val bankIndex = Wire(UInt((config.offsetWidth-2).W))
-  val tag  = Wire(UInt(config.tagWidth.W))
+  val index = Wire(UInt(config.indexWidth.W))
+  val bankIndex = Wire(UInt((config.offsetWidth - 2).W))
+  val tag = Wire(UInt(config.tagWidth.W))
   val waySelReg = RegInit(0.U(config.wayNumWidth.W))
 
   index := config.getIndex(addr_r)
@@ -83,76 +83,78 @@ class ICache(val config:CacheConfig)
 
   tag := config.getTag(addr_r)
 
-//  val dataBankWtMask = WireInit(VecInit.tabulate(4) { _ => true.B })
-//  val writeData =WireInit(VecInit.tabulate(4) { _ => 0.U(8.W) })
+  //  val dataBankWtMask = WireInit(VecInit.tabulate(4) { _ => true.B })
+  //  val writeData =WireInit(VecInit.tabulate(4) { _ => 0.U(8.W) })
   /**
    * dataMem
    */
-  for(way <- 0 until config.wayNum){
+  for (way <- 0 until config.wayNum) {
     tagvMem(way).io.clka := clock
     tagvMem(way).io.wea := tagvData.wEn(way)
     tagvMem(way).io.ena := true.B
-    for(bank <- 0 until config.bankNum){
+    for (bank <- 0 until config.bankNum) {
       dataMem(way)(bank).io.clka := clock
       dataMem(way)(bank).io.wea := bData.wEn(way)(bank)
       dataMem(way)(bank).io.ena := true.B
     }
   }
 
-  for(way <- 0 until config.wayNum){
-    tagvMem(way).io.addra := config.getIndexByExpression(state===sIDLE,io.addr,addr_r)
-    tagvData.read(way).tag := tagvMem(way).io.douta(config.tagWidth-1,0)
+  for (way <- 0 until config.wayNum) {
+    tagvMem(way).io.addra := config.getIndexByExpression(state === sIDLE, io.addr, addr_r)
+    tagvData.read(way).tag := tagvMem(way).io.douta(config.tagWidth - 1, 0)
     tagvData.read(way).valid := tagvMem(way).io.douta(config.tagWidth)
-    for(bank <- 0 until config.bankNum) {
-      dataMem(way)(bank).io.addra := config.getIndexByExpression(state===sIDLE,io.addr,addr_r)
-      bData.read(way)(bank)  := dataMem(way)(bank).io.douta
+    for (bank <- 0 until config.bankNum) {
+      dataMem(way)(bank).io.addra := config.getIndexByExpression(state === sIDLE, io.addr, addr_r)
+      bData.read(way)(bank) := dataMem(way)(bank).io.douta
     }
   }
-//  for(way <- 0 until config.wayNum){
-//    tagvData.read(way) := tagvMem(way).read(config.getIndex(io.addr))
-//  }
-//  for(way <- 0 until config.wayNum ) {
-//    for (bank <- 0 until config.bankNum) {
-//      when(bData.wEn(way)(bank) && state === sREFILL) {
-//        dataMem(way)(bank).write(bData.addr, bData.write(bank))
-//      }
-//    }
-//  }
-  for(way <- 0 until config.wayNum){
-    when(tagvData.wEn(way)){//写使能
-      tagvMem(way).io.dina := Cat(true.B,tag)
-//      tagvMem(way).write(tagvData.addr,tag)
-//      validMem(way)(tagvData.addr) := true.B
+  //  for(way <- 0 until config.wayNum){
+  //    tagvData.read(way) := tagvMem(way).read(config.getIndex(io.addr))
+  //  }
+  //  for(way <- 0 until config.wayNum ) {
+  //    for (bank <- 0 until config.bankNum) {
+  //      when(bData.wEn(way)(bank) && state === sREFILL) {
+  //        dataMem(way)(bank).write(bData.addr, bData.write(bank))
+  //      }
+  //    }
+  //  }
+  for (way <- 0 until config.wayNum) {
+    when(tagvData.wEn(way)) { //写使能
+      tagvMem(way).io.dina := Cat(true.B, tag)
+      //      tagvMem(way).write(tagvData.addr,tag)
+      //      validMem(way)(tagvData.addr) := true.B
     }
-    for(bank <- 0 until config.bankNum) {
-      dataMem(way)(bank).io.dina := io.axi.readData.bits.data
-    }
-  }
-  for(way<- 0 until config.wayNum){
-    for(bank <- 0 until config.bankNum) {
-      bData.wEn(way)(bank) := state===sREFILL && waySelReg === way.U && bDataWtBank ===bank.U
+    for (bank <- 0 until config.bankNum) {
+      dataMem(way)(bank).io.dina := Mux(state===sQueryPrefetch,prefetch.io.query_data(bank),io.axi.readData.bits.data)
     }
   }
-  for(way<- 0 until config.wayNum){
-    tagvData.wEn(way) :=  (state === sREFILL && waySelReg===way.U)
+  for (way <- 0 until config.wayNum) {
+    for (bank <- 0 until config.bankNum) {
+      bData.wEn(way)(bank) := (state === sREFILL && waySelReg === way.U && bDataWtBank === bank.U) || //查询axi回填
+                          (state === sQueryPrefetch && prefetch.io.query_finded && waySelReg===way.U) //预取命中
+    }
+  }
+  for (way <- 0 until config.wayNum) {
+    tagvData.wEn(way) := (state === sREFILL && waySelReg === way.U) ||
+                        (state === sQueryPrefetch && prefetch.io.query_finded && waySelReg===way.U)
   }
 
-//  tagvData.write := tag
-//  bData.addr := index
-//  tagvData.addr := index
+  //  tagvData.write := tag
+  //  bData.addr := index
+  //  tagvData.addr := index
   cache_hit_way := OHToUInt(cache_hit_onehot)
   // 判断是否命中cache
-  cache_hit_onehot.indices.foreach(i=>{
-    cache_hit_onehot(i) :=  tagvData.read(i).tag === tag & tagvData.read(i).valid
+  cache_hit_onehot.indices.foreach(i => {
+    cache_hit_onehot(i) := tagvData.read(i).tag === tag & tagvData.read(i).valid
   })
 
   /**
    * IO初始化
    */
 
-  io.instValid := Cat(bankIndex =/= ((config.bankNum) - 1).U,true.B) //==bank-1
-  io.instOK    := false.B
-//  printf("%d\n",io.instValid)
+  io.instValid := Cat(bankIndex =/= ((config.bankNum) - 1).U, true.B) //==bank-1
+  io.instOK := false.B
+  //  printf("%d\n",io.instValid)
   io.inst := 0.U
 
   /**
@@ -162,28 +164,36 @@ class ICache(val config:CacheConfig)
   lruMem.io.visit := 0.U
   lruMem.io.visitValid := is_hitWay
 
-  /**
-   * reset
-   * 重写数据为0
-   */
-  val resetCounter = Counter(1<<config.indexWidth)
-  resetCounter.inc()
+//  /**
+//   * reset
+//   * 重写数据为0
+//   */
+//  val resetCounter = Counter(1 << config.indexWidth)
+//  resetCounter.inc()
 
   /**
    * axi访问设置
    */
-  io.axi.readAddr.bits.id := Mux(prefetch.io.use_axi,prefetch.io.readAddr.bits.id,0.U)
+  io.axi.readAddr.bits.id := Mux(prefetch.io.use_axi, prefetch.io.readAddr.bits.id, 0.U)
   io.axi.readAddr.bits.len := (config.bankNum - 1).U
   io.axi.readAddr.bits.size := 2.U // 4B
-  io.axi.readAddr.bits.addr := Mux(prefetch.io.use_axi,prefetch.io.readAddr.bits.addr,addr_r)
+  io.axi.readAddr.bits.addr := Mux(prefetch.io.use_axi, prefetch.io.readAddr.bits.addr, addr_r)
   io.axi.readAddr.bits.cache := 0.U
   io.axi.readAddr.bits.lock := 0.U
   io.axi.readAddr.bits.prot := 0.U
   io.axi.readAddr.bits.burst := 2.U //突发模式2
-  io.axi.readAddr.valid := Mux(prefetch.io.use_axi,prefetch.io.readAddr.valid,state===sREPLACE)
+  io.axi.readAddr.valid := Mux(prefetch.io.use_axi, prefetch.io.readAddr.valid, state === sREPLACE)
 
-  io.axi.readData.ready := state===sREFILL | prefetch.io.readData.ready
+  io.axi.readData.ready := state === sREFILL | prefetch.io.readData.ready
 
+  /**
+   * 预取器
+   */
+  prefetch.io.req_valid := state === sREFILL && prefetch.io.req_ready
+  prefetch.io.req_addr := addr_r
+
+  prefetch.io.query_addr := addr_r
+  prefetch.io.query_valid := state === sQueryPrefetch
   prefetch.io.readAddr.ready := io.axi.readAddr.ready
   prefetch.io.readData.bits <> io.axi.readData.bits
   prefetch.io.readData.valid := io.axi.readData.valid
@@ -198,40 +208,33 @@ class ICache(val config:CacheConfig)
   io.debug_hit_count := debug_hit_count_r
   dontTouch(io.debug_total_count)
   dontTouch(io.debug_hit_count)
-  when(state===sLOOKUP){
+  when(state === sLOOKUP) {
     debug_total_count_r := debug_total_count_r + 1.U
-    when(is_hitWay){
+    when(is_hitWay) {
       debug_hit_count_r := debug_hit_count_r + 1.U
     }
   }
 
 
-  /**
-   * 预取器
-   */
-  prefetch.io.req_valid := state === sReqPrefetch && prefetch.io.req_ready
-  prefetch.io.req_addr := addr_r
 
-  prefetch.io.query_addr := addr_r
-  prefetch.io.query_valid := false.B
 
 
   /**
    * Cache状态机
    */
-  switch(state){
-    is(sIDLE){
-      when(io.valid){
+  switch(state) {
+    is(sIDLE) {
+      when(io.valid) {
         state := sLOOKUP
         addr_r := io.addr
       }
     }
-    is(sLOOKUP){
-      when(is_hitWay){
-        when(bankIndex === (config.bankNum - 1).U){
-          io.inst := Cat(0.U(32.W),bData.read(cache_hit_way)(bankIndex))
-        }.otherwise{
-          io.inst := Cat(bData.read(cache_hit_way)(bankIndex+1.U),bData.read(cache_hit_way)(bankIndex))
+    is(sLOOKUP) {
+      when(is_hitWay) {
+        when(bankIndex === (config.bankNum - 1).U) {
+          io.inst := Cat(0.U(32.W), bData.read(cache_hit_way)(bankIndex))
+        }.otherwise {
+          io.inst := Cat(bData.read(cache_hit_way)(bankIndex + 1.U), bData.read(cache_hit_way)(bankIndex))
         }
         io.instOK := true.B
         lruMem.io.visit := cache_hit_way // lru记录命中
@@ -242,83 +245,98 @@ class ICache(val config:CacheConfig)
         }.otherwise {
           state := sIDLE
         }
-//        state := sIDLE
+        //        state := sIDLE
 
       }.otherwise {
         //没命中,尝试请求
         io.inst := 0.U
         io.instOK := false.B
-        state :=  sREPLACE
-//        io.axi.readAddr.valid := true.B
+        state := sQueryPrefetch
+//        prefetch.io.query_valid := true.B // 查询预取器;虽然当拍就能得到结果，但是为了缩短路径，这里延长了一拍
+        waySelReg := lruMem.io.waySel
+        //        io.axi.readAddr.valid := true.B
       }
     }
-    is(sREPLACE){
-      waySelReg := lruMem.io.waySel
-      bDataWtBank := bankIndex
-      when(io.axi.readAddr.ready) {
-        state := sReqPrefetch
+    is(sQueryPrefetch) {
+      // 预取命中
+//      prefetch.io.query_valid := true.B
+      when(prefetch.io.query_finded){
+        //如果找到了
+        state := sIDLE
+        io.instOK := true.B
+        when(bankIndex === (config.bankNum - 1).U) {
+          io.inst := Cat(0.U(32.W), prefetch.io.query_data(bankIndex))
+        }.otherwise {
+          io.inst := Cat(prefetch.io.query_data(bankIndex + 1.U), prefetch.io.query_data(bankIndex))
+        }
       }.otherwise{
         state := sREPLACE
-//        io.axi.readAddr.valid := true.B
       }
     }
-    is(sReqPrefetch){
-      state := RegNext(sREFILL)
+    is(sREPLACE) {
+      // 发起替换请求
+      bDataWtBank := bankIndex
+      when(io.axi.readAddr.ready) {
+        state := sREFILL
+      }.otherwise {
+        state := sREPLACE
+      }
     }
-    is(sREFILL){
-      // 取数据，重写TAGV
+    is(sREFILL) {
+      // 接收数据；同时也会发起预取请求，重写TAGV
       state := sREFILL
-      when(io.axi.readData.valid && io.axi.readData.bits.id === 0.U){
-        bDataWtBank := bDataWtBank+1.U
-        when(bDataWtBank === (bankIndex + 3.U)){
-           // 关键字优先
-          when(bankIndex === (config.bankNum - 1).U){
-            io.inst := Cat(0.U(32.W),bData.read(cache_hit_way)(bankIndex))
-          }.otherwise{
-            io.inst := Cat(bData.read(cache_hit_way)(bankIndex+1.U),bData.read(cache_hit_way)(bankIndex))
+      when(io.axi.readData.valid && io.axi.readData.bits.id === 0.U) {
+        bDataWtBank := bDataWtBank + 1.U
+        when(bDataWtBank === (bankIndex + 3.U)) {
+          // 关键字优先
+          when(bankIndex === (config.bankNum - 1).U) {
+            io.inst := Cat(0.U(32.W), bData.read(cache_hit_way)(bankIndex))
+          }.otherwise {
+            io.inst := Cat(bData.read(cache_hit_way)(bankIndex + 1.U), bData.read(cache_hit_way)(bankIndex))
           }
           io.instOK := true.B
         }
-        when(io.axi.readData.bits.last){
+        when(io.axi.readData.bits.last) {
           state := sIDLE
         }
       }
     }
-//    is(sWaiting){
-//      state := sIDLE
-//      io.inst := Cat(bData.read(waySelReg)(bankIndex+1.U),bData.read(waySelReg)(bankIndex))
-////      io.inst(31,0) := bData.read(waySelReg)(bankIndex)
-////      io.inst(63,32) := bData.read(waySelReg)(bankIndex+1.U)
-//      io.instOK := true.B
-//    }
+    //    is(sWaiting){
+    //      state := sIDLE
+    //      io.inst := Cat(bData.read(waySelReg)(bankIndex+1.U),bData.read(waySelReg)(bankIndex))
+    ////      io.inst(31,0) := bData.read(waySelReg)(bankIndex)
+    ////      io.inst(63,32) := bData.read(waySelReg)(bankIndex+1.U)
+    //      io.instOK := true.B
+    //    }
   }
 
-//  when(io.axi.readAddr.fire()){
-//    io.axi.readAddr.valid := false.B
-//  }
+  //  when(io.axi.readAddr.fire()){
+  //    io.axi.readAddr.valid := false.B
+  //  }
 
 
 }
-
 
 
 class BankData(val config: CacheConfig) extends Bundle {
   //  val addr = Wire(Vec(2,UInt(config.indexWidth.W)))
-  val read = Wire(Vec(config.wayNum, Vec(config.bankNum,UInt(32.W))))
+  val read = Wire(Vec(config.wayNum, Vec(config.bankNum, UInt(32.W))))
   //  val write = Vec(config.bankNum, UInt(32.W))
-  val wEn = Wire(Vec(config.wayNum, Vec(config.bankNum,Bool())))
+  val wEn = Wire(Vec(config.wayNum, Vec(config.bankNum, Bool())))
 }
 
-class tagv(tagWidth:Int) extends Bundle{
+class tagv(tagWidth: Int) extends Bundle {
   val tag = UInt(tagWidth.W)
   val valid = Bool()
 }
+
 class TAGVData(val config: CacheConfig) extends Bundle {
   //  val addr = Wire(Vec(2,UInt(config.indexWidth.W)))
   val read = Wire(Vec(config.wayNum, new tagv(config.tagWidth))) //一次读n个)
-//  val write = Wire(UInt((config.tagWidth+1).W)) //一次写1个
+  //  val write = Wire(UInt((config.tagWidth+1).W)) //一次写1个
   val wEn = Wire(Vec(config.wayNum, Bool()))
 }
+
 object ICache extends App {
   new ChiselStage execute(args, Seq(ChiselGeneratorAnnotation(
     () =>
--- src/main/scala/com/github/hectormips/cache/icache/Prefetch.scala
+++ src/main/scala/com/github/hectormips/cache/icache/Prefetch.scala
@@ -8,85 +8,79 @@ import com.github.hectormips.cache.setting.CacheConfig
 import scala.collection.immutable.Nil
 
 
-class BufferItem(bankNum:Int) extends Bundle{
-  val valid = Bool()
-  val addr  = UInt(32.W)
-  val bank  = Vec(bankNum,UInt(32.W))
+class Buffer(config: CacheConfig) extends Bundle {
+  //
+  val data = RegInit(VecInit(Seq.fill(config.prefetch_buffer_size)(VecInit(Seq.fill(config.bankNum)(0.U(32.W))))))
+  val addr = RegInit(VecInit(Seq.fill(config.prefetch_buffer_size)(0.U(32.W))))
+  val valid = RegInit(VecInit(Seq.fill(config.prefetch_buffer_size)(false.B)))
+  val ptr = Counter(config.prefetch_buffer_size)
 }
-class Buffer(config:CacheConfig)extends Bundle{
-  val data   = RegInit(VecInit(Seq.fill(config.prefetch_buffer_size)({
-    val bundle = Wire(new BufferItem(bankNum = config.bankNum))
-    bundle.valid := 0.U
-    bundle.addr := 0.U
-    for(i <- 0 until config.bankNum){
-      bundle.bank(i) := 0.U
-    }
-    bundle
-  })))
-  val ptr = RegInit(0.U(log2Ceil(config.prefetch_buffer_size)))
-}
+
 /**
  * N+1行预取
  */
-class Prefetch(config:CacheConfig) extends Module{
-  val io = IO(new Bundle{
+class Prefetch(config: CacheConfig) extends Module {
+  val io = IO(new Bundle {
     /**
      * 预取请求
      * 如果预取器正忙，会忽略请求
      */
     val req_valid = Input(Bool())
     val req_ready = Output(Bool())
-    val req_addr =Input(UInt(32.W))
+    val req_addr = Input(UInt(32.W))
 
     /**
-     *  预取查询
+     * 预取查询
      */
-    val query_valid  = Input(Bool())
-    val query_addr   = Input(UInt(32.W))
+    val query_valid = Input(Bool())
+    val query_addr = Input(UInt(32.W))
     val query_finded = Output(Bool())
-    val query_data   = Output(Vec(config.bankNum,UInt(32.W)))
+    val query_data = Output(Vec(config.bankNum, UInt(32.W)))
 
     /**
      * axi
      */
-    val use_axi   = Output(Bool())
-    val readAddr  =  Decoupled(new AXIAddr(32,4))
-    val readData  = Flipped(Decoupled(new AXIReadData(32,4)))
+    val use_axi = Output(Bool())
+    val readAddr = Decoupled(new AXIAddr(32, 4))
+    val readData = Flipped(Decoupled(new AXIReadData(32, 4)))
   })
-  val sIDLE::sHANDSHAKE::sREFILL::Nil = Enum(3)
+  val sIDLE :: sHANDSHAKE :: sREFILL :: Nil = Enum(3)
   val state = RegInit(0.U(2.W))
   val buffer = new Buffer(config)
+//  dontTouch(buffer.data)
   val addr_r = RegInit(0.U(32.W))
   val bankCounter = RegInit(0.U(config.bankNumWidth.W))
 
   /**
    * 预取阶段
    */
-  buffer.data(buffer.ptr).bank(bankCounter) := io.readData.bits.data
+  buffer.data(buffer.ptr.value)(bankCounter) := io.readData.bits.data
   io.use_axi := state === sHANDSHAKE
   io.req_ready := state === sIDLE
-  when(state === sIDLE && io.req_valid && io.req_ready){
-    addr_r := Cat(io.req_addr(31,config.offsetWidth),0.U(config.offsetWidth.W)) + (config.bankNum*4).U
+  when(state === sIDLE && io.req_valid && io.req_ready) {
+    addr_r := Cat(io.req_addr(31, config.offsetWidth), 0.U(config.offsetWidth.W)) + (config.bankNum * 4).U
     state := sHANDSHAKE
   }
-  when(state === sHANDSHAKE){
-    when(io.readAddr.valid && io.readAddr.ready){
+  when(state === sHANDSHAKE) {
+    when(io.readAddr.valid && io.readAddr.ready) {
       state := sREFILL
       bankCounter := 0.U
-    }.otherwise{
+    }.otherwise {
       state := sHANDSHAKE
     }
   }
-  when(state === sREFILL){
-    when(io.readData.valid && io.readData.ready && io.readData.bits.id===1.U){
+  when(state === sREFILL) {
+    when(io.readData.valid && io.readData.ready && io.readData.bits.id === 1.U) {
       bankCounter := bankCounter + 1.U
-      when(io.readData.bits.last){
+      when(io.readData.bits.last) {
         state := sIDLE
-        buffer.ptr := buffer.ptr + 1.U
-      }.otherwise{
+        buffer.ptr.inc()
+        buffer.valid(buffer.ptr.value) := true.B
+        buffer.addr(buffer.ptr.value) := addr_r
+      }.otherwise {
         state := sREFILL
       }
-    }.otherwise{
+    }.otherwise {
       state := sREFILL
     }
   }
@@ -94,23 +88,24 @@ class Prefetch(config:CacheConfig) extends Module{
   /**
    * 查询阶段
    */
-  val query_onehot = Wire(Vec(config.prefetch_buffer_size,Bool()))
-  val query_hit    = Wire(UInt(log2Ceil(config.prefetch_buffer_size).W))
+  val query_onehot = Wire(Vec(config.prefetch_buffer_size, Bool()))
+  val query_hit = Wire(UInt(log2Ceil(config.prefetch_buffer_size).W))
   query_hit := OHToUInt(query_onehot)
-  for (i <- 0 until config.prefetch_buffer_size){
-    query_onehot(i) := buffer.data(i).valid && buffer.data(i).addr(31,config.offsetWidth)===io.query_addr(31,config.offsetWidth)
+  for (i <- 0 until 2) {
+    query_onehot(i) := buffer.valid(i) && buffer.addr(i)(31, config.offsetWidth) === io.query_addr(31, config.offsetWidth)
   }
-  io.query_finded := query_onehot.asUInt() =/= 0.U  && io.query_valid
-  io.query_data := buffer.data(query_hit).bank
+  io.query_finded := query_onehot.asUInt().orR() && io.query_valid
+  dontTouch(io.query_finded)
+  io.query_data := buffer.data(query_hit)
 
 
   /**
    * axi访问设置
    */
   dontTouch(io.readAddr.bits.id)
-//  dontTouch(io.readData.bits.data)
-//  dontTouch(io.readData.valid)
-//  dontTouch(io.readData.ready)
+  //  dontTouch(io.readData.bits.data)
+  //  dontTouch(io.readData.valid)
+  //  dontTouch(io.readData.ready)
   io.readAddr.bits.id := 1.U
   io.readAddr.bits.len := (config.bankNum - 1).U
   io.readAddr.bits.size := 2.U // 4B
@@ -120,5 +115,5 @@ class Prefetch(config:CacheConfig) extends Module{
   io.readAddr.bits.prot := 0.U
   io.readAddr.bits.burst := 1.U //突发模式2
   io.readAddr.valid := state === sHANDSHAKE
-  io.readData.ready := state === sREFILL  //ready最多持续一拍
+  io.readData.ready := state === sREFILL //ready最多持续一拍
 }
