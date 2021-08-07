56e62aca67b3241da5d7cc4c70a5c359daf72dfb
ruiqurm <ruiqurm@gmail.com>
2021-08-07 16:30:15 +0800

    修复uncahce inst的问题

--- src/main/scala/com/github/hectormips/cache/uncache/UncacheInst.scala
+++ src/main/scala/com/github/hectormips/cache/uncache/UncacheInst.scala
@@ -17,13 +17,13 @@ class UncacheInst extends Module{
     val input = Flipped(new SRamLikeInstIO())
     val axi = new AXIIO(1)
   })
-  val sIDLE::s1Handshake::s1Recv::s2Handshake::s2Recv::Nil = Enum(5)
+  val sIDLE::s1Handshake::s1Recv::s2Handshake::s2Recv::sWait::Nil = Enum(6)
   val state = RegInit(0.U(3.W))
   val addr_r = RegInit(0.U(32.W))
   val rdata  = Reg(Vec(2,UInt(32.W)))
   io.input.addr_ok := state === sIDLE
   io.input.data_ok := false.B
-  io.input.rdata := Cat(io.axi.rdata,rdata(0))
+  io.input.rdata := Cat(rdata(1),rdata(0))
   io.input.inst_valid := Cat(true.B,true.B)
   io.input.inst_pc := addr_r
   io.input.inst_predict_jump_in := DontCare
@@ -34,37 +34,40 @@ class UncacheInst extends Module{
   }
   when(state === s1Handshake){
     when(io.axi.arvalid===true.B && io.axi.arready===true.B && io.axi.arid === 0.U){
-      state := s2Handshake
+      state := s1Recv
     }.otherwise{
       state := s1Handshake
     }
   }
   when(state === s2Handshake){
-    when(io.axi.arvalid===true.B && io.axi.arready===true.B && io.axi.arid === 0.U){
-      state := s1Recv
+    when(io.axi.arvalid===true.B && io.axi.arready===true.B && io.axi.arid === 1.U){
+      state := s2Recv
     }.otherwise{
       state := s2Handshake
     }
   }
   when(state === s1Recv){
     when(io.axi.rvalid.asBool() && io.axi.rready.asBool() && io.axi.rid ===0.U){
-      state := s2Recv
+      state := s2Handshake
       rdata(0) :=  io.axi.rdata
     }.otherwise{
       state := s1Recv
     }
   }
   when(state === s2Recv){
-    when(io.axi.rvalid.asBool() && io.axi.rready.asBool()  && io.axi.rid ===0.U){
-      state := sIDLE
-      io.input.data_ok := true.B
+    when(io.axi.rvalid.asBool() && io.axi.rready.asBool()  && io.axi.rid ===1.U){
+      state := sWait
+      rdata(1) :=  io.axi.rdata
     }.otherwise{
       state := s2Recv
     }
   }
-
+  when(state === sWait){
+    state := sIDLE
+    io.input.data_ok := true.B
+  }
   //ar
-  io.axi.arid := 0.U
+  io.axi.arid := Mux(state===s2Handshake,1.U,0.U)
   io.axi.araddr := Mux(state===s1Handshake,addr_r,addr_r+4.U)
   io.axi.arlen := 0.U
   io.axi.arsize := 3.U
@@ -75,7 +78,7 @@ class UncacheInst extends Module{
   io.axi.arvalid := (state === s1Handshake || state === s2Handshake)
 
   //r
-  io.axi.rready := 1.U
+  io.axi.rready := state===s1Recv && io.axi.rid ===0.U || state===s2Recv && io.axi.rid === 1.U
 
 
 
